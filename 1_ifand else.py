871ce9c234ede94022b7d75002a4aafd5e19becc
naidu1 <mallemputi135@gmail.com>
2022-09-25 15:24:22 +0530

    this is my first commit

---
+++ 1_ifand else.py
@@ -0,0 +1,29 @@
+# if is a block of code this only execute when statement is true
+# a=2
+# if a==2:
+#     print("ok")
+
+# else is a block of code this only execute when statement if clause is failed
+
+b=5
+if b==6:
+    print("true")
+else:
+    print("not true")
+
+# elif : is a block of code when execute this if clause is failed and also elif block is true then
+# execute that, we can write elif condition any number of times
+
+# o=7
+# h=8
+# if o==h:
+#     print('ok')
+# elif o>h:
+#     print("o is greater than h")
+# elif o<h:
+#     print("o is less than h")
+# elif o<=h:
+#     print("o is less than are equal to h")
+# else:
+#     print("else block is executed")
+
---
+++ break,continue,pass.py
@@ -0,0 +1,19 @@
+# # Break:: statement in terminates the current loop and resumes execution at the next statement
+# for i in range(10):
+#     # print(i)
+#     if i ==5:
+#         break
+#     print(i)
+
+
+# continue :: skip the current iteration or loop
+# for j in range(15):
+#     if j==12:
+#         continue
+#     print(j,end=" ")
+
+# pass:: pass can do nothing, only avoid syntax error
+# for k in range(15):
+#     if k==10:
+#         pass
+#     print(k,end=" ")
